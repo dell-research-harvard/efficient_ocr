304ac324ceba1d48610420b1c2d9e04a3f3b5342
jscarlson <jcarlson238@gmail.com>
2023-08-05 17:45:24 -0400

    dev

--- tests/effocr/test_train_recognizer_cgis.py
+++ tests/effocr/test_train_recognizer_cgis.py
@@ -2,8 +2,8 @@ from efficient_ocr import EffOCR
 
 def test_recog_train():
 
-    data_json = "./data/coco_ex.json"
-    config_json = "./config/config_ex.json"
+    data_json = "../../data/coco_ex.json"
+    config_json = "../../config/config_ex.json"
     data_dir = '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/ocr_datasets/locca/images'
 
     effocr = EffOCR(

6ded3a888093ff570e7bca58547b2378a2f0d5ed
jscarlson <jcarlson238@gmail.com>
2023-08-06 17:37:36 -0400

    new test

---
+++ tests/effocr/test_train_recognition_cgis_datajson.py
@@ -0,0 +1,16 @@
+from efficient_ocr import EffOCR
+
+def test_recog_train():
+
+    data_json = "./coco_annotations_multi/all.json"
+    data_dir = './coco_annotations_multi/images'
+    config_yaml = "./config/config_en_datajson.yaml"
+    
+    effocr = EffOCR(
+        data_json, data_dir, config_yaml
+    )
+
+    effocr.train(target=['char_recognition'])
+
+if __name__ == '__main__':
+    test_recog_train()

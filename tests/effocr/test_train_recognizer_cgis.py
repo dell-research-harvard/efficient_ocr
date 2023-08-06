be0db0decbb3d8c8486eb814a0b6b73464f768ce
jscarlson <jcarlson238@gmail.com>
2023-08-06 11:36:56 -0400

    New jp test

--- tests/effocr/test_train_recognizer_cgis.py
+++ tests/effocr/test_train_recognizer_cgis.py
@@ -4,13 +4,13 @@ def test_recog_train():
 
     data_json = "../../data/coco_ex.json"
     data_dir = '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/ocr_datasets/locca/images'
-    config_yaml = "../../config/config_ex.yaml"
+    config_yaml = "../../config/config_jp_ex.yaml"
     
     effocr = EffOCR(
         data_json, data_dir, config_yaml
     )
 
-    effocr.train(target=['char_recognition', 'word_recognition'])
+    effocr.train(target=['char_recognition'])
 
 if __name__ == '__main__':
     test_recog_train()

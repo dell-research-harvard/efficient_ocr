45f85621606fbfc62652306b56d67d5bb6336c33
bryanptom <bryanptom@gmail.com>
2023-08-24 14:17:20 -0400

    current imports on detector factory

--- src/efficient_ocr/detection/models/factory.py
+++ src/efficient_ocr/detection/models/factory.py
@@ -3,9 +3,9 @@ Detector Model Factory Class
 
 Generates model class based on config file. Model classes are subclasses 
 '''
-from onnx import YOLOv5ModelONNX, YOLOv8ModelONNX, MMDetectModelONNX
-from yolo import YOLOv5Model, YOLOv8Model
-from mmdetect import MMDetectModel
+# from onnx import YOLOv5ModelONNX, YOLOv8ModelONNX, MMDetectModelONNX
+from .yolo import YOLOv5Model, YOLOv8Model
+# from mmdetect import MMDetectModel
 
 def DetectorFactory(config, target):
 
@@ -14,13 +14,13 @@ def DetectorFactory(config, target):
             return YOLOv5Model
         elif config[target]['model_backend'] == 'yolov8':
             return YOLOv8Model
-        elif config[target]['model_backend'] == 'mmdetection':
-            return MMDetectModel
-    elif config[target]['onnx']:
-        if config[target]['model_backend'] == 'yolov5':
-            return YOLOv5ModelONNX
-        elif config[target]['model_backend'] == 'yolov8':
-            return YOLOv8ModelONNX
-        elif config[target]['model_backend'] == 'mmdetection':
-            return MMDetectModelONNX
+        # elif config[target]['model_backend'] == 'mmdetection':
+        #     return MMDetectModel
+    # elif config[target]['onnx']:
+    #     if config[target]['model_backend'] == 'yolov5':
+    #         return YOLOv5ModelONNX
+    #     elif config[target]['model_backend'] == 'yolov8':
+    #         return YOLOv8ModelONNX
+    #     elif config[target]['model_backend'] == 'mmdetection':
+    #         return MMDetectModelONNX
     
